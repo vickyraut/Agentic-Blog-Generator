56161a4a8f870e4df990459e48e790af1e35971c
Codemonk <78207195+vickyraut@users.noreply.github.com>
2026-08-05 17:37:41 +0530

    Language Translation Graph Build

--- src/agentic_blog_generator/graphs/graphBuilder.py
+++ src/agentic_blog_generator/graphs/graphBuilder.py
@@ -13,11 +13,11 @@ class GraphBuilder:
         Build a graph to generate blogss based on topic
         """
 
-        obj_blogNode = BlogNode(self.llm)
+        self.obj_blogNode = BlogNode(self.llm)
 
         #Nodes
-        self.graph.add_node("title_creation",obj_blogNode.title_creation)
-        self.graph.add_node("content_generator", obj_blogNode.content_generation)
+        self.graph.add_node("title_creation",self.obj_blogNode.title_creation)
+        self.graph.add_node("content_generator", self.obj_blogNode.content_generation)
 
         #Edges
         self.graph.add_edge(START, "title_creation")
@@ -26,9 +26,46 @@ class GraphBuilder:
 
         return self.graph
 
+    def build_language_graph(self):
+        """
+        Build a graph for blog generation with inputs topic and language
+        """
+
+        self.obj_blogNode = BlogNode(self.llm)
+        
+        #Nodes
+        self.graph.add_node("title_creation",self.obj_blogNode.title_creation)
+        self.graph.add_node("content_generator", self.obj_blogNode.content_generation)
+        self.graph.add_node("hindi_translation",)
+        self.graph.add_node("french_translation",)
+        self.graph.add_node("route",)
+
+        #Edges
+        self.graph.add_edge(START, "title_creation")
+        self.graph.add_edge("title_creation", "content_generation")
+        self.graph.add_edge("content_generation", "route")
+
+        #Conditional Edge
+        self.graph.add_conditional_edges(
+            "route",
+            self.obj_blogNode.route_decision,
+            {
+                "hindi":"hindi_translation",
+                "french":"french_translation"
+            }
+        )
+
+        self.graph.add_edge("hindi_translation",END)
+        self.graph.add_edge("french_translation",END)
+        return self.graph
+
+
     def setupGraph(self, usecase):
         if usecase == "topic":
             self.build_topic_graph()
+        if usecase=="language":
+            print("Language block")
+            self.build_language_graph()
 
         return self.graph.compile()
 
